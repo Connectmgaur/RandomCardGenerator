837fe31b1f84b65581f3c593c5603b9a61427b96
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-08-21 19:52:39 +0000

    Second Commit

--- src/app.js
+++ src/app.js
@@ -4,38 +4,40 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-//write your code here
-//window.onload = function() {
 
-// console.log("Everything is working");
-//};
-// Function to shuffle the card
-//};
+function shuffleCard() {
+  const suits = ["spade", "club", "heart", "diamond"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 
-window.onload = function() {
-  function shuffleCard() {
-    const suits = ["spade", "club", "heart", "diamond"];
-    const numbers = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
+  let suitIndex = getRandomNumber(0, 3);
+  let numberIndex = getRandomNumber(0, 12);
 
-    let suitIndex = getRandomNumber(0, 3);
-    let numberIndex = getRandomNumber(0, 12);
+  let card = document.querySelector(".card");
+  card.className = "card " + suits[suitIndex];
+  card.querySelector(".number").textContent = numbers[numberIndex];
+}
 
-    let card = document.querySelector(".card");
-    card.className = "card " + suits[suitIndex];
-    card.textContent = numbers[numberIndex];
-  }
-};
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//shuffleCard(); // Shuffle the card on initial page load
+//const shuffleButton = document.getElementById("shuffleButton");
+
+window.onload = shuffleCard();
+let ok = document.querySelector(".shuffleButton");
+ok.addEventListener("click", shuffleCard);
