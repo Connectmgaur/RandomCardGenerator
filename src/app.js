e6136d34b400b0a8579ed609bce2dc2d8fa3165c
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-08-21 19:16:04 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,38 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+//write your code here
+//window.onload = function() {
+
+// console.log("Everything is working");
+//};
+// Function to shuffle the card
+//};
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function shuffleCard() {
+    const suits = ["spade", "club", "heart", "diamond"];
+    const numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    let suitIndex = getRandomNumber(0, 3);
+    let numberIndex = getRandomNumber(0, 12);
+
+    let card = document.querySelector(".card");
+    card.className = "card " + suits[suitIndex];
+    card.textContent = numbers[numberIndex];
+  }
 };
